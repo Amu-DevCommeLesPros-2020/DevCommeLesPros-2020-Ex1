134a5009760852ddb3be7c8c2a280418038b2bee
Thierry Seegers <surfintheusa@yahoo.com>
2019-11-21 16:45:16 +0100

    Add an incorrect palindrome fonction.
    Add a basic TEST macro the increments the programs return code when a test fails.

--- main.c
+++ main.c
@@ -1,4 +1,54 @@
+#include <stdbool.h>
+#include <string.h>
+
+#define TEST(x) if(!x)              \
+                {                   \
+                    resultat += 1;  \
+                }
+
+bool palindrome(const char* const chaine)
+{
+    size_t debut = 0;
+    size_t fin = strlen(chaine);
+    
+    while(debut != fin)
+    {
+        char const gauche = chaine[debut];
+        char const droite = chaine[fin];    
+        if(gauche != droite)
+        {
+            return false;
+        }
+
+        ++debut;
+        --fin;
+    }
+    
+    return true;
+}
+
 int main()
-{    
-    return 0;
+{
+    // Tous les tests incrémenterons cette variable de 1.
+    // Le but est de la garder à 0.
+    int resultat = 0;
+
+    // Tests de la fonction palindrome.
+    // Tests positifs.
+    TEST(palindrome(""));
+    TEST(palindrome("a"));
+    TEST(palindrome("aa"));
+    TEST(palindrome("aaa"));
+    TEST(palindrome("aba"));
+    TEST(palindrome("aaaa"));
+    TEST(palindrome("abba"));
+    TEST(palindrome(" "));
+    // Tests negatifs.
+    TEST(!palindrome(" a"));
+    TEST(!palindrome("ab"));
+    TEST(!palindrome("aab"));
+
+
+
+    return resultat;
 }
